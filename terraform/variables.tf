c3ee457bb77a03be5700522b2275a57aa43df65c
Ignacio Segura <49452681+IgnaGS@users.noreply.github.com>
2026-04-16 03:46:58 +0000

    update variable default

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "project_id" {
   description = "GCP project ID"
   type        = string
-  default     = "flight-price-analytics"
+  default     = "flight-prices-analytics"
 }
 
 variable "region" {
